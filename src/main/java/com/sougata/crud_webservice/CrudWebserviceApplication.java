30e820fc9ba084c9715788c0fcf4bc6cb6111bb3
Sougata Ghosh <sougataghosh314@gmail.com>
2021-02-04 20:57:44 +0530

    added multipart file upload and download, single or multiple files

--- src/main/java/com/sougata/crud_webservice/CrudWebserviceApplication.java
+++ src/main/java/com/sougata/crud_webservice/CrudWebserviceApplication.java
@@ -1,9 +1,13 @@
 package com.sougata.crud_webservice;
 
+import com.sougata.crud_webservice.properties.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 @SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class })
 public class CrudWebserviceApplication {
 
